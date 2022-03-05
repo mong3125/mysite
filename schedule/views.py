d26289525d67b06c085e1b357d9c2ad2cfc534a7
Daegil Kim <mong3125@naver.com>
2022-03-05 13:30:53 +0900

    마감일 별로 content 묶어서 출력하기

--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from collections import defaultdict
 from .forms import TodoForm
+
+
 # Create your views here.
 
 
@@ -11,7 +14,11 @@ def index(request):
         return render(request, 'common/login.html')
 
     # 일정 출력
-    todo_list = request.user.todo_set.all()
+    todo_list = request.user.todo_set.order_by('deadline').all()
+    todo_list_deadline = defaultdict(list)
+    for todo in todo_list:
+        todo_list_deadline[todo.deadline].append(todo)
+    deadline_list = list(todo_list_deadline.keys())
 
     # 일정 작성
     if request.method == 'POST':
@@ -23,4 +30,7 @@ def index(request):
             return redirect('schedule:index')
     form = TodoForm()
 
-    return render(request, 'schedule/index.html', {'form' : form, 'todo_list' : todo_list})
+    return render(request, 'schedule/index.html', {'form': form,
+                                                   'todo_list': todo_list,
+                                                   'todo_list_deadline': todo_list_deadline,
+                                                   'deadline_list': deadline_list})
