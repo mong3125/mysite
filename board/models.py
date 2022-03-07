cbfe5f9edaef1e8c83cd9233893fab2a1b961ccc
Daegil Kim <mong3125@naver.com>
2022-03-07 23:17:48 +0900

    일정 목록 html, css 수정

--- board/models.py
+++ board/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Post(models.Model):
     WEATHER = {('sunny', '맑음'), ('cloudy', '구름'), ('rainy', '비'), ('snow', '눈'), ('storm', '폭풍'), ('destroy', '멸망')}
 
@@ -11,6 +12,7 @@ class Post(models.Model):
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
     feelingWeather = models.CharField(max_length=10, choices=WEATHER, null=True)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
